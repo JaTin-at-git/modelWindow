6a05577571b962e99d8ed187bb6a750dbb104f83
jatin-at-git <jigyashusaini7@gmail.com>
2021-03-28 14:10:39 +0530

    adding modal modifiers

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 'use strict';
 
+let showModelBtn = document.querySelectorAll('.show-modal');
+let modal = document.querySelector('.modal');
+let overlay = document.querySelector('.overlay');
+let closeModal = document.querySelector('.close-modal');
+
+for (var i = 0; i < showModelBtn.length; i++) {
+  showModelBtn[i].addEventListener('click', function() {
+    overlay.classList.remove('hidden');
+    modal.classList.remove('hidden');
+  });
+}
+
+[closeModal, overlay].forEach(item => {
+  item.addEventListener('click', function() {
+    overlay.classList.add('hidden');
+    modal.classList.add('hidden');
+  });
+});
+
+document.querySelector("body").addEventListener("keydown",function(e){
+  if(e.key=="escape" && !modal.classList.contains('hidden')){
+    alert(e.key);
+    overlay.classList.add('hidden');
+    modal.classList.add('hidden');
+  }
+})
